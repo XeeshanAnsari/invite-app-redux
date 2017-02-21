f67df148febb4c39bb50cdbea66b60dc38961a9d
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 08:32:59 -0800

    invite-reducer working

---
+++ reducers/invite_reducer.js
@@ -0,0 +1,19 @@
+import ActionTypes from '../constants/action_types';
+
+export function inviteReducer(state = {}, action) {
+  switch(action.type) {
+      case ActionTypes.AddToInviteFulfilled: {
+        const newState = Object.assign({}, state, {
+            inProgress: false,
+            success: 'Added guest.'
+        });
+      }
+
+
+       newState.guests = newState.guests || [];
+        newState.guests = newState.guests.slice();
+        newState.guests.push(action.guest);
+        return newState;
+
+  }
+}
