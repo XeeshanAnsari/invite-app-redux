dd217269338b2408af571c0644660f713057e7c5
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 08:56:20 -0800

    invite.jsx working modified

--- componets/invite.jsx
+++ componets/invite.jsx
@@ -13,27 +13,57 @@ export default class Invite extends React.Component {
         return (
             <div className="container">
                 <div className="well">
-                <h1>Meeting invite</h1>
-                </div>
-                <div className="bg-warning meeting-summary">
-                <div className="row">
-                    <div className="col-sm-4 col-md-2">
-                    <b>Host:</b>
+                    <h1>Meeting invite</h1>
                     </div>
-                    <div className="col-sm-8 col-md-10">
-                    {host}
+                    <div className="bg-warning meeting-summary">
+                        <div className="row">
+                            <div className="col-sm-4 col-md-2">
+                            <b>Host:</b>
+                            </div>
+                            <div className="col-sm-8 col-md-10">
+                            {host}
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-4 col-md-2">
+                            <b>Agenda:</b>
+                            </div>
+                            <div className="col-sm-8 col-md-10">
+                            {agenda}
+                            </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-sm-4 col-md-2">
-                    <b>Agenda:</b>
+                <div className="bg-info meeting-form">
+                    <div className="row">
+                        <div className="col-sm-4 col-md-2">
+                           <b>Nmae :</b> 
+                        </div>
+                        <div className="col-sm-8 col-md-10">
+                           <input  type="text"
+                           value={this.state.name}
+                           onChange={e => this.setState({name: e.target.value})}
+                           /> 
+                        </div>
                     </div>
-                    <div className="col-sm-8 col-md-10">
-                    {agenda}
+                    <div className="row">
+                     <div className="col-sm-4 col-md-2">
+                            <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={() => this.props.onAddToInvite(this.state.name)}
+                            >
+                            I am coming!
+                            </button>
+                        </div>
                     </div>
-                </div>
-                </div>
+                 </div>
             </div>
+
+
+    
+
+
+            
 
         )
     }
