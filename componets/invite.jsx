a407fcd44af703a88b989277f3386a963769c6fc
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 07:24:04 -0800

    invite  component create

---
+++ componets/invite.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default class Invite extends React.Component {
+
+
+    ComponentDidMount(){
+        this.props.onGetInvite();
+    }
+
+    render(){
+        const {agenda , host} = this.props.invite;
+
+        return (
+            <div className="container">
+    <div className="well">
+      <h1>Meeting invite</h1>
+    </div>
+    <div className="bg-warning meeting-summary">
+      <div className="row">
+        <div className="col-sm-4 col-md-2">
+          <b>Host:</b>
+        </div>
+        <div className="col-sm-8 col-md-10">
+          {host}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4 col-md-2">
+          <b>Agenda:</b>
+        </div>
+        <div className="col-sm-8 col-md-10">
+          {agenda}
+        </div>
+      </div>
+    </div>
+  </div>
+
+        )
+    }
+}
