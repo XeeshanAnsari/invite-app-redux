b9227d61afcf191eb99ced399b017fa3ff3d060f
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 06:29:02 -0800

    app.jsx working

--- app.jsx
+++ app.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import InviteContainer from './containers/invite_container';
+import store from './store/store';
 
-ReactDOM.render(<div>Hello world</div>,
+import "./stylesshetts/main.scss";
+ 
+
+ const main = (
+   <Provider store={store}>
+     <InviteContainer />
+     
+     </Provider >
+ )
+
+ReactDOM.render(main,
   document.getElementById('container'));
