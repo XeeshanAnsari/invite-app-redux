2d95f320b9018e3ab0957204273469eef5eb44f4
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 10:27:53 -0800

    guest added event when guest is add

---
+++ actions/guest_added_event.js
@@ -0,0 +1,18 @@
+import ActionTypes from '../constants/action_types';
+
+import database from './database';
+
+
+export function watchGuestAddedEvent(dispatch){
+    database.ref('/guest').on('child_added' , (snap) => {
+        dispatch(getGuestAddedAction(snap.val()));
+    });
+
+}
+
+function getGuestAddedAction(guest){
+    return{
+        type:ActionTypes.GuestAdded,
+        guest
+    };
+}
