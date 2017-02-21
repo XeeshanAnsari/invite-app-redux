cd2e4f5068afdd6b704ba9153ec1294703fd3fb0
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 07:37:54 -0800

    add_invite using fulfilled , rejected , requested

---
+++ actions/add_invite.js
@@ -0,0 +1,39 @@
+import ActionTypes from '../constants/action_types';
+import database from './database';
+
+export function addToInvite(name) {
+    return dispatch => {
+        dispatch(addToInviteRequestAction());
+
+        const guestsRef = database.ref('/guests');
+        guestsRef.push ({
+            name
+        })
+        .then(()=>{
+            dispatch(addToInviteFulfilledAction({name}));
+        })
+        .catch((error) =>{
+            dispatch(addToInviteInviteRejectedAction());
+        })
+    }
+}
+
+
+function addToInviteRequestedAction() {
+  return {
+    type: ActionTypes.AddToInviteRequested
+  };
+}
+
+function addToInviteRejectedAction() {
+  return {
+    type: ActionTypes.AddToInviteRejected
+  }
+}
+
+function addToInviteFulfilledAction(){
+    return {
+        type: ActionType.AddToInviteFulfilled,
+        guest
+    }
+}
--- componets/invite.jsx
+++ componets/invite.jsx
@@ -12,28 +12,28 @@ export default class Invite extends React.Component {
 
         return (
             <div className="container">
-    <div className="well">
-      <h1>Meeting invite</h1>
-    </div>
-    <div className="bg-warning meeting-summary">
-      <div className="row">
-        <div className="col-sm-4 col-md-2">
-          <b>Host:</b>
-        </div>
-        <div className="col-sm-8 col-md-10">
-          {host}
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4 col-md-2">
-          <b>Agenda:</b>
-        </div>
-        <div className="col-sm-8 col-md-10">
-          {agenda}
-        </div>
-      </div>
-    </div>
-  </div>
+                <div className="well">
+                <h1>Meeting invite</h1>
+                </div>
+                <div className="bg-warning meeting-summary">
+                <div className="row">
+                    <div className="col-sm-4 col-md-2">
+                    <b>Host:</b>
+                    </div>
+                    <div className="col-sm-8 col-md-10">
+                    {host}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-4 col-md-2">
+                    <b>Agenda:</b>
+                    </div>
+                    <div className="col-sm-8 col-md-10">
+                    {agenda}
+                    </div>
+                </div>
+                </div>
+            </div>
 
         )
     }
