14ad946df185c0b080a418b60895143894f36c3d
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 11:03:20 -0800

    database.js modified like apikey , url and domain set

--- actions/database.js
+++ actions/database.js
@@ -1,10 +1,15 @@
 import firebase from 'firebase';
 
-const config = {
-    apiKey: '',
-    authDomain:'',
-    databaseURL:''
-}
+
+ const config = {
+    apiKey: "AIzaSyAWZgS3WzRJWThOb8b8bGdSLi8OLuoFeEg",
+    authDomain: "reactapp-7fce4.firebaseapp.com",
+    databaseURL: "https://reactapp-7fce4.firebaseio.com",
+    storageBucket: "reactapp-7fce4.appspot.com",
+    messagingSenderId: "804525042836"
+  }
+
+
 firebase.initializeApp(config);
 const database  = firebase.database();
 
