80fa0f47c7c6fadde5da4bc281d8e9940257f0e2
xeeshan <zeeshanansari38244@gmail.com>
2017-02-21 10:38:22 -0800

    invite reducer for newly guest

--- reducers/invite_reducer.js
+++ reducers/invite_reducer.js
@@ -2,18 +2,24 @@ import ActionTypes from '../constants/action_types';
 
 export function inviteReducer(state = {}, action) {
   switch(action.type) {
+      
       case ActionTypes.AddToInviteFulfilled: {
-        const newState = Object.assign({}, state, {
-            inProgress: false,
-            success: 'Added guest.'
-        });
-      }
-
-
-       newState.guests = newState.guests || [];
-        newState.guests = newState.guests.slice();
-        newState.guests.push(action.guest);
-        return newState;
-
+      const newState = Object.assign({}, state, {
+        inProgress: false,
+        success: 'Added guest.'
+      });
+      // newState.guests = newState.guests || [];
+      // newState.guests = newState.guests.slice();
+      // newState.guests.push(action.guest);
+      return newState;
+    }
+    case ActionTypes.GuestAdded: {
+      const newState = Object.assign({}, state);
+      newState.guests = newState.guests || [];
+      newState.guests = newState.guests.slice();
+      newState.guests.push(action.guest);
+      return newState;
+    }
+ 
   }
 }
